1eccf95d1db2af5cc63e3aef39824ee3fa31cc01
KishanPatel1674 <kishanbhupendrabhai.patel@edu.sait.ca>
2025-09-26 15:31:03 -0600

    test

--- app/about/page.js
+++ app/about/page.js
@@ -1,5 +1,5 @@
 "use client"
-
+//this is about page for a freelancing marketplace website.
 import React from 'react';
 import { Users, Target, Award, Globe, Heart, Zap } from 'lucide-react';
 import Link from 'next/link';
