78257ba54a8e8acf4ef2e6c5d9dbeed5aba2c5c9
KishanPatel1674 <kishanbhupendrabhai.patel@edu.sait.ca>
2025-11-16 17:38:07 -0700

    add the reference link in themeprovider.tsx

--- app/components/ThemeProvider.tsx
+++ app/components/ThemeProvider.tsx
@@ -1,3 +1,22 @@
+/**
+ * ThemeProvider Component
+ *
+ * This component wraps your application with NextThemesProvider from "next-themes".
+ * It enables theme management (like light/dark mode) across the app.
+ *
+ * Usage:
+ *   Wrap your app's root layout with <ThemeProvider> to apply theme support.
+ *
+ * Props:
+ *   children - The child components that will have access to the theme context.
+ *   ...props  - Any other props supported by NextThemesProvider (like defaultTheme, attribute, etc.)
+ *
+ * Example:
+ *   <ThemeProvider attribute="class" defaultTheme="system">
+ *     <App />
+ *   </ThemeProvider>
+ */
+
 "use client";
 
 import * as React from "react";
